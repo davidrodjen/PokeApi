e9764962f0035ab4bd48863bbdaa7a1e9a1cae0a
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-04-27 08:22:24 -0700

    Set up main program and methods for calling API

---
+++ PokeApiConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using PokeApiCore;
+using System;
+using System.Threading.Tasks;
+
+namespace PokeApiConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            PokeApiClient client = new PokeApiClient();
+            string result = await client.GetPokemonByName("bulbasaur");
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
--- PokeApiCore/PokeApiClient.cs
+++ PokeApiCore/PokeApiClient.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace PokeApiCore
 {
@@ -8,14 +10,36 @@ namespace PokeApiCore
     /// Client class to consume PokeApi
     /// https://pokeapi.co/
     /// Had to add Nuget pckg, System.net.http;
+    /// MAKE ONLY ONE HTTPCLIENT
     /// </summary>
     public class PokeApiClient
     {
         static readonly HttpClient client = new HttpClient();
 
-        public void GetPokemonByName(string name)
+        /// <summary>
+        /// Retrieve Pokemon by Name
+        /// Catch isn't necessary but keeping it
+        /// </summary>
+        /// <exception cref="HttpRequestException"></exception>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task<string> GetPokemonByName(string name)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string url = $"https://pokeapi.co/api/v2/pokemon/{name}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync(); //Puts it in a big string
+                return responseBody;
+
+            }
+            catch(HttpRequestException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                throw ex;
+            }
         }
 
         public void GetPokemonById(int id)
