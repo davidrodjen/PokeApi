4c8b187538dffb313fca1e0d0818e5b633d8e7dc
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-04-27 08:00:37 -0700

    Added System.Net.Http, downloaded nuget pckg through dependencies

--- PokeApiCore/PokeApiClient.cs
+++ PokeApiCore/PokeApiClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace PokeApiCore
 {
@@ -6,8 +7,10 @@ namespace PokeApiCore
     /// <summary>
     /// Client class to consume PokeApi
     /// https://pokeapi.co/
+    /// Had to add Nuget pckg, System.net.http;
     /// </summary>
     public class PokeApiClient
     {
+        static readonly HttpClient client = new HttpClient();
     }
 }
